42bfab1fec6018edd4a778f6423e8821de08299d
光弘 <guanghong.wsj@alibaba-inc.com>
2015-07-23 17:14:25 +0800

    改造

--- demo/PickerDemo.js
+++ demo/PickerDemo.js
@@ -6,18 +6,76 @@
  * All rights reserved.
  */
 var classnames = require('classnames');
+var GroupList = require('tingle-group-list');
 
 var Picker = require('../src');
+
+var city = {
+    "b": [
+        {
+            name: '北京',
+            abbr: 'bj',
+            pinyin: 'beijing'
+        }
+    ],
+    "c": [
+        {
+            name: '重庆',
+            abbr: 'cq',
+            pinyin: 'chongqing' 
+        },
+        {
+            name: '长春',
+            abbr: 'ch',
+            pinyin: 'changchun' 
+        }
+    ],
+    "h": [
+        {
+            name: '杭州',
+            abbr: 'hz',
+            pinyin: 'hangzhou'
+        }
+    ],
+    "s": [
+        {
+            name: '上海',
+            abbr: 'sh',
+            pinyin: 'shanghai' 
+        }
+    ],
+    "n": [
+        {
+            name: '南京',
+            abbr: 'nj',
+            pinyin: 'nanjing'
+        }
+    ],
+    "w": [
+        {
+            name: '武汉',
+            abbr: 'wh',
+            pinyin: 'wuhan'
+        }
+    ]
+}
 
 class Demo extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            city: ['长春', '北京', '上海', '广州', '长沙', '武汉', '南京', '天津', '石家庄'],
-            height: 'auto'
+            city: {},
+            height: 'auto',
+            show: false
         };
         var t = this;
+    }
+
+    componentWillMount() {
+        var t = this;
+        t.city = city;
+        t.filterList('');
     }
 
     componentDidMount() {
@@ -28,22 +86,83 @@ class Demo extends React.Component {
         })
     }
 
-    handlePick(data) {
-        alert(data.getAttribute('data-name'));
+    _renderGroupLists() {
+        var t = this;
+        var arr = []
+        var groups = Object.keys(t.state.city);
+        groups.forEach(function(group, i) {
+            arr.push(<GroupList className={group} title={group.toUpperCase()} key={group}>
+                        {t.state.city[group].map(function(city, i) {
+                            return <div className="tLH44 tPL10" key={group + city.pinyin} data-group={group} data-name={city.name} onClick={t.handleClick.bind(t)}>{city.name}</div>
+                        })}
+                    </GroupList>)
+        });
+        return arr;
+    }
 
+    handleClick(e) {
+        var t = this;
+        var target = e.currentTarget;
+        var name = target.getAttribute('data-name');
+        var group = target.getAttribute('data-group');
+        console.log(t.state.city[group].filter(function(ele) {
+            return ele.name == name;
+        })[0]);
+
+    }
+
+    handleSearch(value) {
+        var t = this;
+        t.filterList(value);
+    }
+
+    /**
+     * 判断是否是汉字
+     */
+
+    _isChinese(str) {
+        return /^[\u4e00-\u9fa5]+$/.test(str);
+    }
+
+    filterList(value) {
+        var t = this;
+        var city = t.city;
+        var groups = Object.keys(city);
+        var newCity = {};
+        if (t._isChinese(value)) {
+            groups.forEach(function(group, i) {
+                var groupCity = city[group].filter(function(ele) {
+                    return ele.name.indexOf(value) != -1
+                })
+                if (groupCity.length != 0) {
+                    newCity[group] = groupCity;
+                }
+            });
+        }
+        else {
+            groups.forEach(function(group, i) {
+                var groupCity = city[group].filter(function(ele) {
+                    return ele.abbr.indexOf(value) != -1 || ele.pinyin.indexOf(value) != -1
+                })
+                if (groupCity.length != 0) {
+                    newCity[group] = groupCity;
+                }
+            });
+        }
+        t.setState({
+            city: newCity
+        })
     }
 
     render() {
         var t = this;
         return (
             <div className="pickerDemo" style={{
-                height: t.state.height
+                display: t.state.show ? 'block' : 'none'
             }}>
-                <div className="filterBG"></div>
-                <Picker onPick={t.handlePick.bind(t)}>
-                    {t.state.city.map(function(ele, i) {
-                        return <div className="pickerItem tFS16" key={ele} data-name={ele}>{ele}</div>
-                    })}
+                <div className="button"></div>
+                <Picker onSearch={t.handleSearch.bind(t)}>
+                    {t._renderGroupLists()}
                 </Picker>
             </div>
         );
--- src/Picker.js
+++ src/Picker.js
@@ -12,8 +12,6 @@ var Scroller =  require('tingle-scroller');
 var SearchBar = require('tingle-search-bar');
 var Context = require('tingle-context');
 
-// https://github.com/hotoo/pinyin
-var pinyin = require('pinyin');
 
 // ES6 析构赋值
 var {is, support, TOUCH, noop} = Context;
@@ -27,18 +25,9 @@ class Picker extends React.Component {
         super(props);
         var t = this;
         t.state = {
-            list: [],
-            keyList: [],
-            height: 'auto',
-            filter: props.filter
+
         };
 
-    }
-
-    componentWillMount() {
-        var t = this;
-        t.sortList = t._sortList(t.props.children);
-        t._changeList();
     }
 
     componentDidMount() {
@@ -49,176 +38,29 @@ class Picker extends React.Component {
         });
         t.scroller = t.refs.scroller.scroller;
         var scroller = React.findDOMNode(t.refs.scroller);
-        scroller.style.height = height - 88 + 'px';
+        scroller.style.height = height - 44 + 'px';
         t.scroller.refresh();        
     }
 
     componentWillReceiveProps() {
         var t = this;
-        t.sortList = t._sortList(t.props.children);
-        t._changeList();
-
     }
 
-    handleEvent(e) {
-        var t = this;
-
-    }
-
-    /**
-     * 列表按子元素排列
-     * @param {Array} children props.children
-     * @return {Array} 有序列表，按字典序排列
-     */
-
-    _sortList(children) {
-        var t = this;
-        return children.sort(function(a, b) {
-            var aIni = a.props['data-name']
-            var bIni = b.props['data-name'];
-            [aIni, bIni] = [aIni, bIni].map(function(ini, i) {
-                return t._getInitial(ini)
-            });
-
-            return aIni.localeCompare(bIni);
-        });
-    }
-
-    /**
-     * 按关键字过滤列表
-     * @param {Array} list React Element 组成的有序列表
-     * @return {Array} 过滤后的列表
-     */
-
-    _filterList(list) {
-        var t = this;
-        var filter = t.state.filter;
-        var filterIsChinese = t._isChinese(filter);
-        return list.filter(function(ele) {
-            var name = ele.props['data-name'];
-            if (filterIsChinese) {
-                return name.indexOf(filter) > -1
-            }
-            else {
-                if (t._isChinese(name)) {
-                    name = t._nameToPinyin(name);
-                }
-                return name.indexOf(filter) > -1
-            }
-        });
-    }
-
-    /**
-     * 讲汉字转化成拼音全拼
-     * @param {String} str 汉字
-     * @return {String} 拼音
-     */
-
-    _nameToPinyin(str) {
-        var py = pinyin(str, {
-                segment: true,
-                style: pinyin.STYLE_NORMAL
-            });
-        py.map(function(ele, i) {
-            return ele[0];
-        });
-        var str = py.join('');
-        return str;
-    }
-
-    /**
-     * 判断是否是汉字
-     */
-
-    _isChinese(str) {
-        return /^[\u4e00-\u9fa5]+$/.test(str);
-    }
-
-    /**
-     * 获取首写字母
-     * @param {String} 要获取首字母的汉字或者英文
-     */
-
-    _getInitial(str) {
-        var t = this;
-        if (t._isChinese(str)) {
-            str = t._nameToPinyin(str)[0]
-        }
-        else {
-            str = str[0]
-        }
-        return str;
-    }
-
-    _changeList() {
-        var t = this;
-        t.filterList = t._filterList(t.sortList);
-        t.keyList = t.filterList.map(function(ele, i) {
-            var name = ele.props['data-name'];
-            return t._getInitial(name);
-        });
-        t.keyList = t._dupRemove(t.keyList);
-        t.setState({
-            list: t.filterList,
-            keyList : t.keyList
-        });
-    }
-
-    _dupRemove(arr) {
-        var newArr = [];
-        return arr.filter(function(ele) {
-            if (newArr.indexOf(ele) > -1) {
-                return false;
-            }
-            else {
-                newArr.push(ele);
-                return true;
-            }
-        });
-    }
-
-    _handleItemClick(e) {
-        var t = this;
-        var target = e.currentTarget;
-        t.props.onPick(target);
-    }
 
     _handleKeyChange(value) {
         var t = this;
-        // setState 是异步执行的，所以要用回调函数。
         t.setState({
             filter: value
-        }, t._changeList.bind(t));
+        }, function() {
+            t.props.onSearch(value);
+        });
 
     }
 
-    _renderItems() {
+    _renderItems(){
         var t = this;
-        var items = [];
-        t.state.keyList.forEach(function(ele, i) {
-            var arr = [];
-
-            arr.push(<div className="tItemTitle tFCf tFS20 tLH30" key={"tItemTitle" + i}>{ele.toUpperCase()}</div>)
-            arr.push(<div className="tItemList" key={"tItemList" + i}>
-                        {t.state.list.filter(function(a) {
-                            return t._getInitial(a.props['data-name']) == ele
-                        }).map(function(e, j) {
-                            var newE = React.cloneElement(e, {
-                                onClick: t._handleItemClick.bind(t)
-                            });
-                            return newE;
-                        })}
-                    </div>)
-            items.push(<div className={classnames({
-                tPickerItem: true,
-                last: i == t.state.keyList.length - 1
-            })} key={ele}>
-                        {arr}
-                       </div>); 
-        })
-        return items
+        return t.props.children;
     }
-
 
     render() {
         var t = this;
@@ -229,7 +71,7 @@ class Picker extends React.Component {
             })} style={{
                 height: t.state.height
             }}>
-                <SearchBar placeholder='搜索' value={t.state.filter} onChange={t._handleKeyChange.bind(t)}/>
+                <SearchBar placeholder='中文/拼音/首字母' value={t.state.filter} onChange={t._handleKeyChange.bind(t)}/>
                 <Scroller ref="scroller" bounce={false}>
                     {t._renderItems()}
                 </Scroller>
@@ -240,7 +82,6 @@ class Picker extends React.Component {
 
 Picker.defaultProps = {
     showKeyBar: false,
-    onPick: noop,
     onSearch: noop,
     onScroll: noop
 }
@@ -248,7 +89,6 @@ Picker.defaultProps = {
 // http://facebook.github.io/react/docs/reusable-components.html
 Picker.propTypes = {
     showKeyBar: React.PropTypes.bool,
-    onPick: React.PropTypes.func,
     onSearch: React.PropTypes.func,
     onScroll: React.PropTypes.func
 }
