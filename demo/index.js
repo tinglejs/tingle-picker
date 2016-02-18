ccb7453c3a3db5ed50145ec9b4d1a4f9fb15ca3d
光弘 <guanghong.wsj@alibaba-inc.com>
2016-02-18 17:48:01 +0800

    delete to update

--- demo/index.js
+++
@@ -1,6 +0,0 @@
-var Context = require('tingle-context');
-var Fastclick = require('fastclick');
-Fastclick.attach(document.body);
-var Demo = require('./PickerDemo');
-
-React.render(<Demo/>, document.getElementById('TingleDemo'));
--- src/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * Picker Component for tingle
- * @author guanghong.wsj
- *
- * Copyright 2014-2015, Tingle Team, Alinw.
- * All rights reserved.
- */
-module.exports = require('./Picker');
